e9ad3758b35d134a2aeede668d2737c7550b5f92
Ymiboza <sprigan.uchiha@gmail.com>
2023-12-08 15:07:59 +0300

    other fix

--- start.js
+++ start.js
@@ -1,22 +1,8 @@
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 
-// Запуск сервера
-const serverProcess = exec('npm start', { cwd: 'js-advanced-diploma' });
+const serverProcess = spawn('npm', ['start'], { cwd: 'js-advanced-diploma' });
+const clientProcess = spawn('npm', ['start'], { cwd: 'js-advanced-diploma-client' });
 
-// Запуск клиента
-const clientProcess = exec('npm start', { cwd: 'js-advanced-diploma-client' });
-
-// Обработка события завершения процесса сервера
-serverProcess.on('exit', (code, signal) => {
-  console.log(`Server process exited with code ${code} and signal ${signal}`);
-});
-
-// Обработка события завершения процесса клиента
-clientProcess.on('exit', (code, signal) => {
-  console.log(`Client process exited with code ${code} and signal ${signal}`);
-});
-
-// Обработка события завершения скрипта
 process.on('exit', () => {
   serverProcess.kill();
   clientProcess.kill();
