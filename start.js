726ff666d170686ae695cd582509549b9636090e
Ymiboza <sprigan.uchiha@gmail.com>
2023-12-08 15:07:48 +0300

    other fix

--- start.js
+++ start.js
@@ -9,19 +9,15 @@ const clientProcess = exec('npm start', { cwd: 'js-advanced-diploma-client' });
 // Обработка события завершения процесса сервера
 serverProcess.on('exit', (code, signal) => {
   console.log(`Server process exited with code ${code} and signal ${signal}`);
-  // Здесь вы можете выполнить дополнительные действия при завершении сервера
 });
 
 // Обработка события завершения процесса клиента
 clientProcess.on('exit', (code, signal) => {
   console.log(`Client process exited with code ${code} and signal ${signal}`);
-  // Здесь вы можете выполнить дополнительные действия при завершении клиента
 });
 
 // Обработка события завершения скрипта
 process.on('exit', () => {
-  // Здесь вы можете выполнить дополнительные действия при завершении скрипта
-  // Например, завершить процессы сервера и клиента
   serverProcess.kill();
   clientProcess.kill();
 });
